dcc4be91066d6049fb2e36591f7757519e00030f
abramo0 <azerabramo@gmail.com>
2026-04-25 21:32:50 +0200

    Update missing.py

--- src/metrics/missing.py
+++ src/metrics/missing.py
@@ -1,1 +1,9 @@
 
+def check_missing(df):
+    missing_percentage = df.isnull().mean()
+    
+    result = {}
+    for col, val in missing_percentage.items():
+        result[col] = round(val, 3)
+    
+    return result
