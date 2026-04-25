480b5e5550b6e70982537ed496903f7739b4e59f
abramo0 <azerabramo@gmail.com>
2026-04-25 21:33:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from src.core.loader import load_data
+from src.core.validator import validate
+from src.report.generator import generate_report
 
+
+def main():
+    path = "data/raw/data.csv"  # cambia con il tuo file
+
+    # 1. Load data
+    df = load_data(path)
+
+    # 2. Validate data
+    results = validate(df)
+
+    # 3. Generate report
+    generate_report(results)
+
+
+if __name__ == "__main__":
+    main()
