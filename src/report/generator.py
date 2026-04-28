7ee1627f39b7ebaab5f45bf3b416ae26474ba6a6
abramo0 <azerabramo@gmail.com>
2026-04-28 20:57:20 +0200

    Update generator.py

--- src/report/generator.py
+++ src/report/generator.py
@@ -4,17 +4,6 @@ def generate_report(results):
     # Missing values
     print("Missing Values:")
     for col, val in results["missing"].items():
-        percentage = val["missing_percentage"]
-        status = "OK" if percentage == 0 else "WARNING"
-
-        print(f" - {col}: {percentage}% [{status}]")
-
-    print("\n===========================\n")def generate_report(results):
-    print("\n=== DATA QUALITY REPORT ===\n")
-
-    # Missing values
-    print("Missing Values:")
-    for col, val in results["missing"].items():
         status = "OK" if val == 0 else "WARNING"
         print(f" - {col}: {val*100}% [{status}]")
 
