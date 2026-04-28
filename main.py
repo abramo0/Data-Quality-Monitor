73fa8eacb2f71848904dd50d2b61a957e9dc1511
abramo0 <azerabramo@gmail.com>
2026-04-28 20:29:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-from src.core.loader import load_data
+from src.core.loader import DataLoader
+from src.core.validator import validate
+from src.report.generator import generate_report
+
+
+def main():
+    path = "data/raw/data.csv"
+
+    # 1. Load data
+    loader = DataLoader(path)
+    df = loader.load_csv()
+
+    # sicurezza (importante)
+    if df is None:
+        print("[ERROR] Data loading failed. Exiting...")
+        return
+
+    # 2. Validate data
+    results = validate(df)
+
+    # 3. Generate report
+    generate_report(results)
+
+
+if __name__ == "__main__":
+    main()from src.core.loader import load_data
 from src.core.validator import validate
 from src.report.generator import generate_report
 
