f966d3a3eca039652506184e9c8a73e2301d700f
abramo0 <azerabramo@gmail.com>
2026-05-02 21:15:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from src.utils.logger import get_logger
 
 from src.core.loader import DataLoader
 from src.core.validator import validate
-from src.report.generator import ReportGenerator
 from src.core.score import DataQualityScore
+from src.report.generator import ReportGenerator
 
 logger = get_logger()
 
@@ -20,6 +20,7 @@ def main():
 
     logger.info("Starting Data Quality Monitor...")
 
+    # Load
     loader = DataLoader(args.file)
     df = loader.load_csv()
 
@@ -27,10 +28,21 @@ def main():
         logger.error("Loading failed. Exiting...")
         return
 
+    # Validate
     results = validate(df)
 
+    # Score (QUI è la novità)
+    score_engine = DataQualityScore(results)
+    score = score_engine.compute()
+    status = score_engine.status(score)
+
+    # Report
     report = ReportGenerator(results)
     report.print_report()
+
+    # Output finale
+    print(f"\n⭐ FINAL SCORE: {score}/100")
+    print(f"📊 STATUS: {status}")
 
     logger.info("Execution completed successfully")
 
