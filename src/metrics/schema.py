bad3d4986f90cbdbb07ed10a21c4fc6e14dba742
abramo0 <azerabramo@gmail.com>
2026-05-02 19:43:43 +0200

    Update schema.py

--- src/metrics/schema.py
+++ src/metrics/schema.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+
+class SchemaChecker:
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+
+    def analyze(self):
+        report = {}
+
+        for col in self.df.columns:
+            dtype = str(self.df[col].dtype)
+
+            report[col] = {
+                "dtype": dtype,
+                "is_numeric": pd.api.types.is_numeric_dtype(self.df[col])
+            }
+
+        return report
