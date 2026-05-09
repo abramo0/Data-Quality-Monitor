21118daebb2100097fb3aabe5cf8949d1cfcfcf3
abramo0 <azerabramo@gmail.com>
2026-05-09 22:38:57 +0200

    Update validator.py

--- src/core/validator.py
+++ src/core/validator.py
@@ -11,7 +11,7 @@ def validate(df):
     # --------------------
     # METRICS
     # --------------------
-    missing_checker = MissingsChecker(df)
+    missing_checker = MissingChecker(df)
     results["missing"] = missing_checker.analyze()
     results["missing_summary"] = missing_checker.summary()
 
